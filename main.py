ddfeb2fac60fe9e72a3eb1e51848ef2b36bc2626
Veronica57 <tveronichka@gmail.com>
2021-02-27 00:08:46 +0300

    Interface updated

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ print("You entered two numbers: a = ", a, ", b = ", b)
 print("Choose the operation:")
 print("1.Addition")
 print("2.Multiplication")
-
+c = int(input())
+if (c == 1):
+    print()
+elif (c == 2):
+    print()
