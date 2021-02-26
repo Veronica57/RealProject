310792e040e29c385dc876b0ee25d90f971bc571
Veronica57 <tveronichka@gmail.com>
2021-02-26 23:59:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Enter two integers")
+a = int(input())
+b = int(input())
+print("You entered two numbers: a = ", a, ", b = ", b)
+print("Choose the operation:")
+print("1.Addition")
+print("2.Multiplication")
+
